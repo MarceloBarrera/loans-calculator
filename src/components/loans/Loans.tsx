4e6636e6bb64b4a959b8c7a6e081c74894662cd0
MarceloBarrera <marcelobarrera@048-WSLT.local>
2021-10-07 09:02:04 +0100

    loans - adding amount and duration to the state parent component

--- src/components/loans/Loans.tsx
+++ src/components/loans/Loans.tsx
@@ -1,26 +1,55 @@
+import { useState } from "react";
 import BusinessLoans from "./businessLoan/BusinessLoan";
-import "./Loans.css";
 import RevolvingCreditFacility from "./revolvingCreditFacility/RevolvingCreditFacility";
-const Loans = () => (
-  <>
-    <div className="container-loan">
-      <h3>Your loan</h3>
-      <div className="loan-request">
-        <label htmlFor="amountRequested">Amount requested:</label>
-        <input type="number" id="amountRequested" defaultValue="10"></input>
-        <span>(in £)</span>
+import "./Loans.css";
 
-        <label htmlFor="durationRequested">Duration:</label>
-        <input type="number" id="durationRequested" defaultValue="4"></input>
-        <span>(in months)</span>
-      </div>
+const Loans = () => {
+  const [amount, setAmount] = useState<number>(10000);
+  const [duration, setDuration] = useState<number>(4);
 
-      <div className="loan-calculations">
-        <RevolvingCreditFacility amountRequested={10000} duration={4} />
-        <BusinessLoans amountRequested={10000} duration={4} />
+  const handleAmount = (event: any) => {
+    setAmount(parseInt(event.target.value));
+  };
+
+  const handleDuration = (event: any) => {
+    setDuration(parseInt(event.target.value));
+  };
+
+  return (
+    <>
+      <div className="container-loan">
+        <h3>Your loan</h3>
+
+        <div className="loan-request">
+          <label htmlFor="amountRequested">Amount requested:</label>
+          <input
+            type="number"
+            id="amountRequested"
+            value={amount}
+            onChange={handleAmount}
+          ></input>
+          <span>(in £)</span>
+
+          <label htmlFor="durationRequested">Duration:</label>
+          <input
+            type="number"
+            id="durationRequested"
+            value={duration}
+            onChange={handleDuration}
+          ></input>
+          <span>(in months)</span>
+        </div>
+
+        <div className="loan-calculations">
+          <RevolvingCreditFacility
+            amountRequested={amount}
+            duration={duration}
+          />
+          <BusinessLoans amountRequested={10000} duration={4} />
+        </div>
       </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
 
 export default Loans;
